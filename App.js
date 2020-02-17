c47c06e35a6b42f44864e7916dfb0f1f1468ac78
Piotrek <piotras86@gmail.com>
2020-02-17 20:50:35 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.listen(3000, () => console.log("Server Up"));
+
+
+mongoose.connect("mongodb://localhost:27017/users", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = mongoose.Schema({
+    fName: String,
+    lName: String,
+    email: String,
+    city: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+// const newUser = new User ({
+//     fName: "jan",
+//     lName: "marian",
+//     email: "piotr@wp.pl",
+//     city: "Ojnice"
+//   });
+
+// newUser.save();
+
+app.get("/", (req, res) => {
+    res.send("ok");
+})
