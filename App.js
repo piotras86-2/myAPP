d70308a251db58534851235ac35f4a99ff9edfc8
Piotrek <piotras86@gmail.com>
2020-02-17 21:26:14 +0100

    I've added post req

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require ("body-parser");
 
 app.listen(3000, () => console.log("Server Up"));
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/users", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -27,4 +29,9 @@ const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.sendFile('public/index.html', {root: __dirname});
+})
+
+app.post("/", (req,res) => {
+    console.log(req.body.fName, req.body.lName, req.body.email, req.body.city);
+    res.redirect("/");
 })
